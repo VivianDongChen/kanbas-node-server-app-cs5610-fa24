e725fd7528553fd4932edcdf23b4831dea201161
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-21 22:47:00 -0800

    1

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -4,3 +4,9 @@ export function enrollUserInCourse(userId, courseId) {
   const { enrollments } = Database;
   enrollments.push({ _id: Date.now(), user: userId, course: courseId });
 }
+
+export function unenrollUserInCourse(userId, courseId) {
+  const { enrollments } = Database;
+  const enrollmentUnenrolled = enrollments.find((enrollment) => enrollment.user === userId && enrollment.course === courseId);
+  Database.enrollments = enrollments.filter((enrollment) => enrollment._id !== enrollmentUnenrolled._id);
+}

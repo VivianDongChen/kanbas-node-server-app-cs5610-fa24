2e87739987b78afddda73f428690a4e5245b7872
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-22 16:07:33 -0800

    1

--- Kanbas/Enrollments/routes.js
+++ Kanbas/Enrollments/routes.js
@@ -2,13 +2,13 @@ import * as dao from "./dao.js";
 
 export default function EnrollmentRoutes(app) {
 
-  app.delete("/api/enrollments/", (req, res) => {
+  app.delete("/api/enrollments", (req, res) => {
     const { userId, courseId } = req.params;
     dao.unenrollUserInCourse(userId, courseId);
     res.sendStatus(204);
   });
 
-  app.put("/api/enrollments/", (req, res) => {
+  app.put("/api/enrollments", (req, res) => {
     const { userId, courseId } = req.params;
     dao.enrollUserInCourse(userId, courseId);
     res.sendStatus(204);

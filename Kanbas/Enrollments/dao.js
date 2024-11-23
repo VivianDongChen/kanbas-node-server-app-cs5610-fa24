ad5f00fce3af3ab7e443cc342cd3c227defb5d9e
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-23 00:02:52 -0800

    1

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -8,6 +8,11 @@ export function enrollUserInCourse(userId, courseId) {
 export function unenrollUserInCourse(userId, courseId) {
   const { enrollments } = Database;
   const enrollmentUnenrolled = enrollments.find((enrollment) => enrollment.user === userId && enrollment.course === courseId);
+    // 错误可能是 enrollmentUnenrolled 为 undefined，需处理这种情况
+    if (!enrollmentUnenrolled) {
+      console.error(`No enrollment found for user: ${userId} and course: ${courseId}`);
+      return; // 或者抛出一个适当的错误
+    }
   Database.enrollments = enrollments.filter((enrollment) => enrollment._id !== enrollmentUnenrolled._id);
 }
 

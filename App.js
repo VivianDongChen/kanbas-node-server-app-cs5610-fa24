27053bb8441fc0225565110ff5d4d2019b2f5cfe
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-24 14:45:10 -0800

    1

--- App.js
+++ App.js
@@ -9,20 +9,16 @@ import WorkingWithArrays from "./Lab5/WorkingWithArrays.js";
 import cors from "cors";
 import UserRoutes from "./Kanbas/Users/routes.js";
 import session from "express-session";
-import "dotenv/config";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+import mongoose from "mongoose";
+import "dotenv/config";
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: process.env.NETLIFY_URL || "http://localhost:3001",
-//   })
-// );
 
 app.use(
   cors({
--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -18,34 +18,33 @@ export default function UserRoutes(app) {
   const findUserById = (req, res) => {};
   app.get("/api/users/:userId", findUserById);
 
-  const updateUser = (req, res) => {
+  const updateUser = async (req, res) => {
     const userId = req.params.userId;
     const userUpdates = req.body;
-    dao.updateUser(userId, userUpdates);
-    const currentUser = dao.findUserById(userId);
+    await dao.updateUser(userId, userUpdates);
+    const currentUser = await dao.findUserById(userId);
     req.session["currentUser"] = currentUser; // 更新会话中的用户信息
     res.json(currentUser);
   };
   app.put("/api/users/:userId", updateUser);
 
-  const signup = (req, res) => {
-    const user = dao.findUserByUsername(req.body.username);
+  const signup = async (req, res) => {
+    const user = await dao.findUserByUsername(req.body.username);
     if (user) {
       res.status(400).json({ message: "Username already in use" });
       return;
     }
-    const currentUser = dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
     req.session["currentUser"] = currentUser; //将用户存储到会话中
     res.json(currentUser);
   };
   app.post("/api/users/signup", signup);
 
-  const signin = (req, res) => {
+  const signin = async (req, res) => {
     const { username, password } = req.body;
-    const currentUser = dao.findUserByCredentials(username, password);
+    const currentUser = await dao.findUserByCredentials(username, password);
     if (currentUser) {
       req.session["currentUser"] = currentUser; // 将用户存储到会话中
-      console.log("Session after signin:", req.session); // 检查session
       res.json(currentUser);
       return;
     } else {
@@ -54,13 +53,13 @@ export default function UserRoutes(app) {
   };
   app.post("/api/users/signin", signin);
 
-  const signout = (req, res) => {
+  const signout = async (req, res) => {
     req.session.destroy(); // 销毁当前会话
     res.sendStatus(200);
   };
   app.post("/api/users/signout", signout);
 
-  const profile = (req, res) => {
+  const profile = async (req, res) => {
     const currentUser = req.session["currentUser"]; // 从会话中获取当前用户
     if (!currentUser) {
       res.sendStatus(401); // 如果没有用户会话，返回 HTTP 401
@@ -70,7 +69,7 @@ export default function UserRoutes(app) {
   };
   app.post("/api/users/profile", profile);
 
-  const findCoursesForEnrolledUser = (req, res) => {
+  const findCoursesForEnrolledUser = async (req, res) => {
     let { userId } = req.params;
     if (userId === "current") {
       const currentUser = req.session["currentUser"];
@@ -80,15 +79,15 @@ export default function UserRoutes(app) {
       }
       userId = currentUser._id;
     }
-    const courses = courseDao.findCoursesForEnrolledUser(userId);
+    const courses = await courseDao.findCoursesForEnrolledUser(userId);
     res.json(courses);
   };
   app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 
-  const createCourse = (req, res) => {
+  const createCourse = async (req, res) => {
     const currentUser = req.session["currentUser"];
-    const newCourse = courseDao.createCourse(req.body);
-    enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
+    const newCourse = await courseDao.createCourse(req.body);
+    await enrollmentsDao.enrollUserInCourse(currentUser._id, newCourse._id);
     res.json(newCourse);
   };
   app.post("/api/users/current/courses", createCourse);

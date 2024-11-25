71f6f0007892739f01022eb7278218d9b17d0336
VivianDongChen <chen.dong1@northeastern.edu>
2024-11-24 19:00:59 -0800

    1

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -12,7 +12,10 @@ export default function UserRoutes(app) {
   const deleteUser = (req, res) => {};
   app.delete("/api/users/:userId", deleteUser);
 
-  const findAllUsers = (req, res) => {};
+  const findAllUsers = async (req, res) => {
+    const users = await dao.findAllUsers();
+    res.json(users);
+    };
   app.get("/api/users", findAllUsers);
 
   const findUserById = (req, res) => {};

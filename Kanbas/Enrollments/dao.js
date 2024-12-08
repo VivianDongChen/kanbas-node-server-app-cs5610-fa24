8e6f860f99f5399f263f984d52790a0f9aa7948f
VivianDongChen <chen.dong1@northeastern.edu>
2024-12-07 20:54:34 -0800

    5.3.1 declare enrollments as a many to many relationship

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,21 +1,22 @@
-import Database from "../Database/index.js";
+import model from "./model.js";
+
+export async function findCoursesForUser(userId) {
+  const enrollments = await model.find({ user: userId }).populate("course");
+  return enrollments.map((enrollment) => enrollment.course);
+}
+export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
+}
 
 export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  enrollments.push({ _id: Date.now(), user: userId, course: courseId });
+  return model.create({ user, course });
 }
 
 export function unenrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  const enrollmentUnenrolled = enrollments.find((enrollment) => enrollment.user === userId && enrollment.course === courseId);
-    // 错误可能是 enrollmentUnenrolled 为 undefined，需处理这种情况
-    if (!enrollmentUnenrolled) {
-      console.error(`No enrollment found for user: ${userId} and course: ${courseId}`);
-      return; // 或者抛出一个适当的错误
-    }
-  Database.enrollments = enrollments.filter((enrollment) => enrollment._id !== enrollmentUnenrolled._id);
+  return model.deleteOne({ user, course });
 }
 
-export function findUserEnrollments(userId) {
-  return Database.enrollments.filter((enrollment) => enrollment.user === userId);
-}
+// export function findUserEnrollments(userId) {
+//   return Database.enrollments.filter((enrollment) => enrollment.user === userId);
+// }

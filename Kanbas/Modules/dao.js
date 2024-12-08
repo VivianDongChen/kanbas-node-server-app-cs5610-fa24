60aa05f912db2c7dfa133ebd6b4f9441bbbd37fe
VivianDongChen <chen.dong1@northeastern.edu>
2024-12-07 16:10:03 -0800

    5.2.4 delete modules

--- Kanbas/Modules/dao.js
+++ Kanbas/Modules/dao.js
@@ -12,13 +12,14 @@ export function createModule(module) {
   return model.create(module);
 }
 
-
-
-
+//delete modules
 export function deleteModule(moduleId) {
-  const { modules } = Database;
-  Database.modules = modules.filter((module) => module._id !== moduleId);
+  return model.deleteOne({ _id: moduleId });
 }
+
+
+
+
 
 export function updateModule(moduleId, moduleUpdates) {
   const { modules } = Database;
